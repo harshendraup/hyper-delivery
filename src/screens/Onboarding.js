77f37d287cc5ef48ce185f5f6eff22e87dfb6696
Arbaz Khan <iamakonarbaz@gmail.com>
2024-10-28 16:36:27 +0530

    splash screen

--- src/screens/Onboarding.js
+++ src/screens/Onboarding.js
@@ -1,19 +1,67 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import logo from '../asset/logo.png';
+import girlBag from '../asset/girlBag.png';
+import girlWeb from '../asset/girlWeb.png';
+import guyPhoto from '../asset/guyPhoto.png';
+
+const images = [girlBag, girlWeb, guyPhoto];
 
 const Onboarding = () => {
-    const navigation = useNavigation()
+  // const navigation = useNavigation();
+  // onPress={() => navigation.navigate('Onboarding')}
   return (
-    <View>
-      <Text>Onboarding</Text>
-        <TouchableOpacity onPress={()=>navigation.navigate("TabNavigator")}> 
-            <Text>Go to Dashboard</Text>
-        </TouchableOpacity>
+    <View style={styles.container}>
+      <View
+        style={styles.touchable}
+        >
+        <Image source={logo} style={styles.logo} />
+        <View>
+          <Text style={styles.boldText}>Let's Start!</Text>
+          <Text style={styles.subText}>
+            Welcome, Please Enter Your Details!
+          </Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Onboarding
+export default Onboarding;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 25,
+  },
+  touchable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 65,
+    height: 65,
+    marginRight: 10,
+  },
+  boldText: {
+    fontSize: 22,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  subText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  paginationBox: {
+    position: 'absolute',
+    bottom: 10,
+    alignSelf: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,19 +1,47 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import {StyleSheet, Text, Image, View, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import logo from '../asset/logo.png'; 
 
 const Splash = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    const timer = setTimeout(() => {
+      if (isMounted) {
+        navigation.navigate('Onboarding');
+      }
+    }, 1000); // Navigate after 1 second
+
+    return () => {
+      setIsMounted(false);
+      clearTimeout(timer); // Clear the timer on unmount
+    };
+  }, [navigation, isMounted]);
+
   return (
-    <View>
-      <Text>Splash</Text>
-      <TouchableOpacity onPress={()=>navigation.navigate("Onboarding")}> 
-        <Text>Go to Onboarding</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Onboarding')}>
+        <Image source={logo} style={styles.logo} />
       </TouchableOpacity>
-  </View>
-  )
-}
+    </View>
+  );
+};
 
-export default Splash
+export default Splash;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', // Change to your desired background color
+  },
+  logo: {
+    width: 200, // Adjust the width as necessary
+    height: 200, // Adjust the height as necessary
+    marginBottom: 20, // Spacing between image and text
+  },
+});
